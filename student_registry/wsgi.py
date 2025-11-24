ababd5131ae11d70af9a76b3dd00b6397c9e9a49
Ezequiel Villa <villa.ezequiel@uabc.edu.mx>
2025-11-24 10:10:21 -0800

    Fix UNESCO indicators fallback and branding updates

---
+++ student_registry/wsgi.py
@@ -0,0 +1,7 @@
+"""Configuración WSGI para el proyecto."""
+import os
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'student_registry.settings')
+
+application = get_wsgi_application()
